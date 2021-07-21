585b4e01fd104830e3142fb0198a67ed08f6922f
SaN4OuSl <oleksandr.laptiev@nure.ua>
2021-07-21 23:47:39 +0300

    improved work of the main class

--- unit_3_base_types/src/main/java/org/exmaple/Unit3Main.java
+++ unit_3_base_types/src/main/java/org/exmaple/Unit3Main.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 
 public class Unit3Main {
     public static void main(String[] args) {
-        while(true) {
+        boolean continueCheck=true;
+        int NumberOfChoice;
+        while(continueCheck) {
             System.out.println("0. Выход из программы");
             System.out.println("1. Задача 1 про сумму чисел из строки");
             System.out.println("2. Задача 2 про количество одинаковых букв в строке");
             System.out.println("3. Задача 3 про конец урока");
             System.out.print("Введите в консоль пункт который хотите запустить:");
-            int NumberOfChoice;
             Scanner NumberOfChoiceScan = new Scanner(System.in);
             NumberOfChoice=NumberOfChoiceScan.nextInt();
             switch(NumberOfChoice) {
                 case 0:
                     NumberOfChoiceScan.close();
-                    System.exit(0);
+                    continueCheck=false;
                     break;
                 case 1:
                     Task1.task1();
@@ -28,6 +29,8 @@ public class Unit3Main {
                 case 3:
                     Task3.task3();
                     break;
+                default:
+                    System.out.println("Не стучите головой об клавиатуру, выберите вариант из списка!\n");
             }
         }
     }
