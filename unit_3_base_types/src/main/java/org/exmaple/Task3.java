3faeee16a0a85a5c3af1767a5154094f93e12c0d
Iegor-Funtusov <funtushan@gmail.com>
2021-07-25 18:00:42 +0300

    add review.txt

--- unit_3_base_types/src/main/java/org/exmaple/Task3.java
+++ unit_3_base_types/src/main/java/org/exmaple/Task3.java
@@ -3,13 +3,14 @@ package org.exmaple;
 import java.util.Scanner;
 
 public class Task3 {
-    public static void task3(){
+
+    public static void task3() {
         Scanner NumOfLessonScan = new Scanner(System.in);
-        int NumberOfLesson, hour;
-        int TimeOfLesson = 45, TimeOfBigBrake = 15, TimeOfSmallBrake = 5;
+        int numberOfLesson, hour;
+        int timeOfLesson = 45, timeOfBigBrake = 15, timeOfSmallBrake = 5;
         System.out.print("Введите номер урока: ");
-        NumberOfLesson = NumOfLessonScan.nextInt();
-        hour = NumberOfLesson * TimeOfLesson + NumberOfLesson / 2 * TimeOfSmallBrake + (NumberOfLesson - 1) / 2 * TimeOfBigBrake;
+        numberOfLesson = NumOfLessonScan.nextInt();
+        hour = numberOfLesson * timeOfLesson + numberOfLesson / 2 * timeOfSmallBrake + (numberOfLesson - 1) / 2 * timeOfBigBrake;
         System.out.printf("Этот урок заканчивается в: ");
         System.out.println(hour / 60 + 9+":"+hour % 60);
     }
