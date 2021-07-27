59782dda6a044d7cf4be253f786ad4b5c64bb996
user <oleksandr.laptiev@nure.ua>
2021-07-27 20:34:25 +0300

    removed background

--- unit_4_arrays/src/main/java/org/example/ConsoleOutput/OutputBoard.java
+++ unit_4_arrays/src/main/java/org/example/ConsoleOutput/OutputBoard.java
@@ -5,22 +5,19 @@ import org.example.Chess.PrintFigure;
 
 public class OutputBoard {
 
-    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-    public static final String ANSI_RESET = "\u001B[0m";
-
     public static void printBoard(ChessBoard board) {
 
         PrintFigure[][] b = board.getBoardArray();
         System.out.println();
-        System.out.println(ANSI_WHITE_BACKGROUND + "    \t[A]\t[B]\t[C]\t[D]\t[E]\t[F]\t[G]\t[H]     " + ANSI_RESET);
+        System.out.println("    \t[A]\t[B]\t[C]\t[D]\t[E]\t[F]\t[G]\t[H]     ");
         for (int i = 0; i < 8; i++) {
-            System.out.print(ANSI_WHITE_BACKGROUND + "[" + (8 - i) + "]   " + ANSI_RESET);
+            System.out.print("[" + (8 - i) + "]   ");
 
             for (int j = 0; j < 8; j++) {
-                System.out.print(ANSI_WHITE_BACKGROUND + b[i][j].getValue() + ANSI_RESET);
+                System.out.print(b[i][j].getValue());
             }
 
-            System.out.println(ANSI_WHITE_BACKGROUND + "\t [" + (8 - i) + "]" + ANSI_RESET);
+            System.out.println("\t [" + (8 - i) + "]" );
         }
     }
 }
