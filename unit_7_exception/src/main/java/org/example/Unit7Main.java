ea39431fa5380b7d65bdd74a55a417297f90e72a
SaN4OuSl <oleksandr.laptiev@nure.ua>
2021-08-21 15:21:29 +0300

    add instruction

--- unit_7_exception/src/main/java/org/example/Unit7Main.java
+++ unit_7_exception/src/main/java/org/example/Unit7Main.java
@@ -10,6 +10,10 @@ public class Unit7Main {
 
     public static void main(String[] args) {
         boolean endProgram = false;
+        System.out.println("ИНСТРУКЦИЯ:\n" +
+                "1. Выберите формат даты европейская(день-месяц-год), амереканская(месяц-день-год)\n" +
+                "2. После выбора откроется меню с списком возможных форматов даты(в списке представлены не все варианты), после выбора действия (1-3) введите дату в формате который вам нужен,\n" +
+                "программа самостоятельно определит в каком формате вы ввели дату\n");
         while (!endProgram) {
             System.out.println("1. If you are european\n" +
                     "2. If you are american\n" +
